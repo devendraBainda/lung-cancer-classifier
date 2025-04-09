f73c080fab8a1275d736c5e9aa4bdeddaf5b6224
devendraBainda <devendrabainda192@gmail.com>
2025-04-10 00:50:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,5 @@ def predict():
     return render_template('results.html', results=results)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=False, host="0.0.0.0", port=port)
